f082150edf063b90b2f5185b68730a8d62c12cdb
Muslimbee <shaymardonovmuslim27@gmail.com>
2023-09-11 03:19:58 +0500

    3 oy 1chi imtixon

---
+++ java/java.js
@@ -0,0 +1,20 @@
+// window.addEventListener("scroll", function(){
+//     var nav = document.querySelector("nav");
+//     header.classList.toggle("sticty", this.window.scrollY > 0)
+// })
+
+
+
+
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         var scroll = $(window).scrollTop();
+//         if (scroll > 10) {
+//           $("nav").css("background" , "blue");
+//         }
+  
+//         else{
+//             $("nav").css("background" , "rgba(0, 255, 255, 0)");  	
+//         }
+//     })
+//   })
